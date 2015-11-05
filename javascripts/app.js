13abe4bea82e1a026224a22ff7db8527621bf1a3
simonhfrost <simonhfrost@gmail.com>
2015-11-05 20:38:41 +0100

    initial commit

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,14 +3,32 @@
 
 var scene = initialize()
 
-function createCube () {
-  var geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
-  var material = new THREE.MeshNormalMaterial()
+function createSphere () {
+  var geometry = new THREE.SphereGeometry(0.5, 0.5, 0.5)
+  var material = new THREE.MeshPhongMaterial({
+    color: '#55BB55'
+  })
   var mesh = new THREE.Mesh(geometry, material)
-  mesh.rotation.x = 45 * (Math.PI / 180)
-  mesh.rotation.y = 45 * (Math.PI / 180)
   return mesh
 }
 
-var cube = createCube()
-scene.add(cube)
+function createLight() {
+  return new THREE.AmbientLight( 0x999999, 0.5 );
+}
+
+function createDirectionalLight() {
+  var directionalLight = new THREE.DirectionalLight( 0x999999, 0.5 );
+  directionalLight.position.set( 10, 1, 10 );
+  return directionalLight
+}
+
+function rotatePlanet() {
+  planet.rotation.y += 0.01
+}
+
+window.setInterval(rotatePlanet, 20)
+
+var planet = createSphere()
+scene.add(planet)
+scene.add(createLight())
+scene.add(createDirectionalLight())
--- javascripts/initializer.js
+++ javascripts/initializer.js
@@ -5,7 +5,7 @@ function createRenderer () {
   var renderer = new THREE.WebGLRenderer({
     antialias: true
   })
-  renderer.setClearColor(new THREE.Color('lightgrey'), 1)
+  renderer.setClearColor(new THREE.Color('lightblue'), 1)
   renderer.setSize(window.innerWidth, window.innerHeight)
   document.body.appendChild(renderer.domElement)
   return renderer
@@ -13,7 +13,8 @@ function createRenderer () {
 
 function createCamera (renderer) {
   var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 1000)
-  camera.position.z = 2
+  camera.position.z = 5
+  camera.position.y = 3
   // NOTE: unused variable must be assigned for the orbit controller to work
   var controls = new THREE.OrbitControls(camera)
 
