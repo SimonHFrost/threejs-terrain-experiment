5d0fa4b38370bde33497d2e494412954bb4509c9
simonhfrost <simonhfrost@gmail.com>
2018-02-28 13:13:52 +1300

    Draw two sided triangle

--- js/app.js
+++ js/app.js
@@ -8,7 +8,32 @@ const createCube = require('./object-creator.js').createCube
 const output = initialize()
 const scene = output.scene
 
-scene.add(createCube())
-
+// scene.add(createCube())
 scene.add(createAmbientLight())
 scene.add(createDirectionalLight())
+
+
+
+
+const geometry = new THREE.Geometry();
+
+geometry.vertices.push(
+	new THREE.Vector3( -10,  10, 0 ),
+	new THREE.Vector3( -10, -10, 0 ),
+	new THREE.Vector3(  10, -10, 0 )
+);
+
+geometry.faces.push( new THREE.Face3( 0, 1, 2 ), new THREE.Face3( 2, 1, 0 ) );
+geometry.computeBoundingSphere();
+
+const material = new THREE.MeshLambertMaterial({
+  color: '#ed8989',
+  flatShading: true
+})
+
+const shape = new THREE.Mesh(
+  geometry,
+  material
+)
+
+scene.add(shape)
