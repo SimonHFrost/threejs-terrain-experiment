7bb542f62cabe7450e8d593ae2173c73ce1bef00
simonhfrost <simonhfrost@gmail.com>
2018-03-01 14:55:51 +1300

    Make drawTerrain function that takes a grid size as input

--- js/app.js
+++ js/app.js
@@ -14,35 +14,39 @@ const RANDOM_CONSTANT = 0.5
 
 const geometry = new THREE.Geometry();
 
-for (let i = -5; i < 5; i++) {
-  for (let j = -5; j < 5; j++) {
-    let x = i - (RANDOM_CONSTANT / 2) + RANDOM_CONSTANT * Math.random()
-    let z = j - (RANDOM_CONSTANT / 2) + RANDOM_CONSTANT * Math.random()
-    let y = (RANDOM_CONSTANT / 2) + RANDOM_CONSTANT * Math.random()
-    geometry.vertices.push(new THREE.Vector3(x, y, z))
+const drawTerrain = (size) => {
+  for (let i = -(size/2); i < size/2; i++) {
+    for (let j = -(size/2); j < (size/2); j++) {
+      let x = i - (RANDOM_CONSTANT / 2) + RANDOM_CONSTANT * Math.random()
+      let z = j - (RANDOM_CONSTANT / 2) + RANDOM_CONSTANT * Math.random()
+      let y = (RANDOM_CONSTANT / 2) + RANDOM_CONSTANT * Math.random()
+      geometry.vertices.push(new THREE.Vector3(x, y, z))
+    }
   }
+
+  for (let row = 0; row < size - 1; row++) {
+    for (let col = 0; col < size - 1; col++) {
+      let first = row * size + col
+      let second = (row + 1) * size + col
+      let third = row * size + col + 1
+      let fourth = (row + 1) * size + col + 1
+      geometry.faces.push(new THREE.Face3(first, second, third))
+      geometry.faces.push(new THREE.Face3(second, fourth, third))
+    }
+  }
+
+  const material = new THREE.MeshLambertMaterial({
+    color: '#00CC00',
+    wireframe: true
+  })
+
+  return new THREE.Mesh(
+    geometry,
+    material
+  )
 }
 
-for (let row = 0; row < 9; row++) {
-  for (let col = 0; col < 9; col++) {
-    let first = row * 10 + col
-    let second = (row + 1) * 10 + col
-    let third = row * 10 + col + 1
-    let fourth = (row + 1) * 10 + col + 1
-    geometry.faces.push(new THREE.Face3(first, second, third))
-    geometry.faces.push(new THREE.Face3(second, fourth, third))
-  }
-}
-
-const material = new THREE.MeshLambertMaterial({
-  color: '#00CC00',
-  wireframe: true
-})
-
-const mesh = new THREE.Mesh(
-  geometry,
-  material
-)
+const mesh = drawTerrain(20)
 
 setInterval(() => {
   mesh.rotation.y += 0.002
